2b204f85c454b65902a0fe67bf5a7248c69a782c
babdba8 <98627201+babdba8@users.noreply.github.com>
2022-02-06 09:33:32 -0500

    Update addTokens.js

--- global/addTokens.js
+++ global/addTokens.js
@@ -13,10 +13,10 @@ async function getName() {
 };
 
 async function addCurrencies() {
-    const tokens = Number(prompt('How many tokens do you want to add to your account? (500 daily)'));
+    const tokens = Number(prompt('How many tokens do you want to add to your account? (1000 daily)'));
 
-    if (tokens > 500) {
-        alert('You can only add up to 500 tokens daily.');
+    if (tokens > 1000) {
+        alert('You can only add up to 1000 tokens daily.');
     };
 
     const response = await fetch('https://api.blooket.com/api/users/add-rewards', {
@@ -34,7 +34,7 @@ async function addCurrencies() {
     });
 
     if (response.status == 200) {
-        alert(`${tokens} tokens and 300 XP added to your account!`);
+        alert(`${tokens} tokens and 500 XP added to your account!`);
     } else {
         alert('An error occured.');
     };
